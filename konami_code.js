39ef3f8022b1879297f8c2b7f1ca8d58340fce8e
Jennifer Snyder <jlsnyder09@gmail.com>
2018-05-01 20:50:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init(e) {
-  const key = parseInt(e.detail || e.which);
+function init() {
   document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
     if (e.which === 38) {
     alert('You did it!')
     }
